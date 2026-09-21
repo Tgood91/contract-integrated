e690298654b311b6d0d348a2ae9419bf0153916f
Tanaz <157336294+Tgood91@users.noreply.github.com>
2026-09-21 16:05:11 -0600

    Update roles.sol
    updated edit

--- roles.sol
+++ roles.sol
@@ -1,5 +1,23 @@
+Roles:
 bytes32 public constant ADMIN_ROLE;
 bytes32 public constant CURATOR_ROLE;
 bytes32 public constant MODERATOR_ROLE;
 bytes32 public constant VALIDATOR_ROLE;
 bytes32 public constant TREASURY_ROLE;
+Mapping.sol
+User Identity Layer
+Every wallet becomes a warrior profile
+struct Warrior {
+    uint64 createdAt;
+    uint64 lastActivity;
+    uint32 level;
+    uint32 honor;
+    uint32 reputation;
+    uint32 achievements;
+    bool active;
+}
+mapping(address => Warrior) public warriors;
+function registerWarrior();
+function updateActivity(address);
+function levelUp(address);
+
