78d575da73a2852b5271bafefa6a3444bd8f837a
Tanaz <157336294+Tgood91@users.noreply.github.com>
2026-09-21 16:01:37 -0600

    Create roles.sol
    admin
    curator
    moderator
    validator
    treasury

---
+++ roles.sol
@@ -0,0 +1,5 @@
+bytes32 public constant ADMIN_ROLE;
+bytes32 public constant CURATOR_ROLE;
+bytes32 public constant MODERATOR_ROLE;
+bytes32 public constant VALIDATOR_ROLE;
+bytes32 public constant TREASURY_ROLE;
